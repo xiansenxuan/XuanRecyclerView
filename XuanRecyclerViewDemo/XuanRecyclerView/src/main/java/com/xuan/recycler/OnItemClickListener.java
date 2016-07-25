73f1186263527dbc3357b396ae174a78032b7e23
xiansenxuan <spike82623@163.com>
2016-07-25 11:18:12 +0800

    delete

--- XuanRecyclerViewDemo/XuanRecyclerView/src/main/java/com/xuan/recycler/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.xuan.recycler;
-
-import android.view.View;
-
-public interface OnItemClickListener {
-	public void onItemClick(View view, int position);
-
-	public void onItemLongClick(View view, int position);
-}
--- XuanRecyclerViewDemo/xuanrecyclerviewlibrary/src/main/java/com/xuan/recyclerview/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.xuan.recyclerview;
-
-import android.view.View;
-
-public interface OnItemClickListener {
-	public void onItemClick(View view, int position);
-
-	public void onItemLongClick(View view, int position);
-}
